bec950f9d2f711aafc3ee536c92748ad98beb8c9
Mark Baggett <mbagget1@utk.edu>
2016-10-05 10:39:28 -0400

    Add DPLA integration.

---
+++ app/dpla.py
@@ -0,0 +1,43 @@
+import requests
+import math
+import yaml
+
+keys = yaml.load(open('keys.yaml', 'r'))
+key = keys['dpla_key']
+
+def get_results(search_term):
+    url_string = 'http://api.dp.la/v2/items?q={0}&page_size=10&page=1&provider.@id=http://dp.la/api/contributor/' \
+             'tn&api_key={1}'.format(search_term, key)
+    s = requests.get(url_string)
+    results = s.json()
+    count = results['count']
+    pages_of_results = math.ceil(count / 10)
+    return pages_of_results
+
+
+def show_results(current_page, search_term):
+    url_string = 'http://api.dp.la/v2/items?q={0}&page_size=12&page={2}&provider.@id=http://dp.la/api/contributor/' \
+             'tn&api_key={1}'.format(search_term, key, current_page)
+    s = requests.get(url_string)
+    results = s.json()
+    number = current_page * 10 - 9
+    things = []
+    for doc in results['docs']:
+        try:
+            data_provider = doc['dataProvider']
+        except:
+            data_provider = 'Unknown'
+            pass
+        things.append({'link': doc['isShownAt'], 'image': doc['object'], 'dataProvider': data_provider, 'title': doc['sourceResource']['title'][0]})
+        number += 1
+    return things
+
+
+def print_url(current_page, search_term):
+    url_string = 'http://api.dp.la/v2/items?q={0}&page_size=10&page={2}&provider.@id=http://dp.la/api/contributor/' \
+             'tn&api_key={1}'.format(search_term, key, current_page)
+    return url_string
+
+
+if __name__ == "__main__":
+    print(key)
