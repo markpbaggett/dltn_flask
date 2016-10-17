65034eeaebc21f5835f71bef19659b60ece214d0
Mark Baggett <mbagget1@utk.edu>
2016-10-16 21:07:09 -0400

    Add some ugly example faceting.

--- app/dpla.py
+++ app/dpla.py
@@ -5,6 +5,7 @@ import yaml
 keys = yaml.load(open('keys.yaml', 'r'))
 key = keys['dpla_key']
 
+
 def get_results(search_term):
     url_string = 'http://api.dp.la/v2/items?q={0}&page_size=10&page=1&provider.@id=http://dp.la/api/contributor/' \
              'tn&api_key={1}'.format(search_term, key)
@@ -17,10 +18,47 @@ def get_results(search_term):
 
 def show_results(current_page, search_term):
     url_string = 'http://api.dp.la/v2/items?q={0}&page_size=12&page={2}&provider.@id=http://dp.la/api/contributor/' \
-             'tn&api_key={1}'.format(search_term, key, current_page)
+             'tn&api_key={1}&facets=dataProvider'.format(search_term, key, current_page)
     s = requests.get(url_string)
     results = s.json()
     number = current_page * 10 - 9
+    things = []
+    facets = get_facets(results)
+    for doc in results['docs']:
+        try:
+            data_provider = doc['dataProvider']
+        except:
+            data_provider = 'Unknown'
+            pass
+        preview = doc['object']
+        thumbnail = preview.replace('crossroads.rhodes.edu:9090/', 'fedora.crossroadstofreedom.org/')
+        things.append({'link': doc['isShownAt'], 'image': thumbnail, 'dataProvider': data_provider, 'title': doc['sourceResource']['title'][0]})
+        number += 1
+    new_results = [things, facets]
+    return new_results
+
+
+def print_url(current_page, search_term):
+    url_string = 'http://api.dp.la/v2/items?q={0}&page_size=10&page={2}&provider.@id=http://dp.la/api/contributor/' \
+             'tn&api_key={1}'.format(search_term, key, current_page)
+    return url_string
+
+
+def get_facets(data):
+    facets = []
+    for doc in data['facets']['dataProvider']['terms']:
+        place = doc['term']
+        total_results = doc['count']
+        facets.append({'place': place, 'results': total_results})
+    return facets
+
+
+def limit_results(facet, search_string):
+    url_string = 'http://api.dp.la/v2/items?q={0}&page_size=12&provider.@id=http://dp.la/api/contributor/' \
+             'tn&api_key={1}&dataProvider={2}'.format(search_string, key, facet)
+    s = requests.get(url_string)
+    results = s.json()
+    number = 1 * 10 - 9
     things = []
     for doc in results['docs']:
         try:
@@ -32,13 +70,8 @@ def show_results(current_page, search_term):
         thumbnail = preview.replace('crossroads.rhodes.edu:9090/', 'fedora.crossroadstofreedom.org/')
         things.append({'link': doc['isShownAt'], 'image': thumbnail, 'dataProvider': data_provider, 'title': doc['sourceResource']['title'][0]})
         number += 1
-    return things
-
-
-def print_url(current_page, search_term):
-    url_string = 'http://api.dp.la/v2/items?q={0}&page_size=10&page={2}&provider.@id=http://dp.la/api/contributor/' \
-             'tn&api_key={1}'.format(search_term, key, current_page)
-    return url_string
+    new_results = things
+    return new_results
 
 
 if __name__ == "__main__":
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import render_template, request
 from .forms import SearchBox
-from .dpla import get_results, print_url, show_results
+from .dpla import get_results, print_url, show_results, limit_results
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -10,13 +11,28 @@ def index():
     if request.method == 'POST':
         results = get_results(form.searchtext.data)
         url = print_url(1, form.searchtext.data)
-        things = show_results(1, form.searchtext.data)
+        stuff = show_results(1, form.searchtext.data)
+        facets = stuff[1]
+        things = stuff[0]
         return render_template('index.html',
                                title='Results',
                                url=url,
                                results=results,
                                things=things,
-                               form=form)
+                               form=form,
+                               facets=facets,
+                               searchterm=form.searchtext.data)
     return render_template('index.html',
                             title='Home',
+                           form=form)
+
+@app.route('/search=<searchterm>&place=<newfacet>')
+def results(searchterm, newfacet):
+    form = SearchBox()
+    results = limit_results(newfacet, searchterm)
+    print(results)
+    return render_template('index.html',
+                           title='Results',
+                           things=results,
+                           searchterm=searchterm,
                            form=form)
