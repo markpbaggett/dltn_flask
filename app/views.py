de911fbe43b774463f6cdd02130c5abd2ff44aa9
Mark Baggett <mbagget1@utk.edu>
2016-10-18 23:59:56 -0400

    Add new example templates.

--- app/views.py
+++ app/views.py
@@ -26,6 +26,48 @@ def index():
                             title='Home',
                            form=form)
 
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    form = SearchBox()
+    if request.method == 'POST':
+        results = get_results(form.searchtext.data)
+        url = print_url(1, form.searchtext.data)
+        stuff = show_results(1, form.searchtext.data)
+        facets = stuff[1]
+        things = stuff[0]
+        return render_template('index.html',
+                               title='Results',
+                               url=url,
+                               results=results,
+                               things=things,
+                               form=form,
+                               facets=facets,
+                               searchterm=form.searchtext.data)
+    return render_template('about.html',
+                           title='About',
+                           form=form)
+
+@app.route('/governance', methods=['GET', 'POST'])
+def governance():
+    form = SearchBox()
+    if request.method == 'POST':
+        results = get_results(form.searchtext.data)
+        url = print_url(1, form.searchtext.data)
+        stuff = show_results(1, form.searchtext.data)
+        facets = stuff[1]
+        things = stuff[0]
+        return render_template('index.html',
+                               title='Results',
+                               url=url,
+                               results=results,
+                               things=things,
+                               form=form,
+                               facets=facets,
+                               searchterm=form.searchtext.data)
+    return render_template('governance.html',
+                           title='Governance',
+                           form=form)
+
 @app.route('/search=<searchterm>&place=<newfacet>', methods=['GET', 'POST'])
 def results(searchterm, newfacet):
     form = SearchBox()
